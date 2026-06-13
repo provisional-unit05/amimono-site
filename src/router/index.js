71b1f070dab4b50f600544f5d17129d4bb513885
まき <yoheimaki0322@gmail.com>
2026-06-13 12:49:12 +0900

    20260613-1249

--- src/router/index.js
+++ src/router/index.js
@@ -34,4 +34,8 @@ const routes = [
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    return { left: 0, top: 0 };
+  },
 });
